cabcbb6fbbb599b1a7f52d04c928f58e1c51e4e7
pj6444 <pj6444@gmail.com>
2013-11-03 12:16:47 -0600

    Create Frame.java

---
+++ Frame.java
@@ -0,0 +1,35 @@
+package com.patrickfeltes.game;
+
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+
+import com.patrickfeltes.game.graphics.Screen;
+
+public class Frame extends JFrame {
+
+	public Frame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Snake");
+		setResizable(false);
+		
+		init();
+	}
+	
+	public void init() {
+		setLayout(new GridLayout(1, 1, 0, 0));
+		
+		Screen s = new Screen();
+		add(s);
+		
+		pack();
+		
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new Frame();
+	}
+	
+}
